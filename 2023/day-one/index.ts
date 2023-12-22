fd0e86e05a100a1df2a0c55756bc77544ac2eda6
Jacob Lamb <jacobtlamb@hey.com>
2023-12-22 10:13:00 -0800

    Update index.ts

--- 2023/day-one/index.ts
+++ 2023/day-one/index.ts
@@ -3,64 +3,36 @@ import input from "./input.txt";
 
 export const processInput = (input: string): number => {
 	const lines = input.split("\n").filter((line) => line.trim() !== "");
-	const allNumbers = [];
-	const pattern: { [key: string]: string } = {
-		one: "1",
-		two: "2",
-		three: "3",
-		four: "4",
-		five: "5",
-		six: "6",
-		seven: "7",
-		eight: "8",
-		nine: "9",
-	};
-
-	function isNumeric(str: string) {
-		return !Number.isNaN(+str);
+	function isDigit(str: string) {
+		return /^\d+$/.test(str);
 	}
-
-	function replaceWrittenNumbersWithDigits(line: string): string {
-		return line.replace(
-			/one|two|three|four|five|six|seven|eight|nine/gi,
-			(matched) => {
-				return pattern[matched.toLowerCase()] + matched[matched.length - 1];
-			},
-		);
-	}
-
-	function replaceWrittenNumbersWithDigitsTwice(line: string): string {
-		const firstPass = replaceWrittenNumbersWithDigits(line);
-		return firstPass.replace(
-			/one|two|three|four|five|six|seven|eight|nine/gi,
-			(matched) => {
-				return pattern[matched.toLowerCase()];
-			},
-		);
-	}
-
-	const convertedLines = lines.map(replaceWrittenNumbersWithDigitsTwice);
-
-	for (let i = 0; i < convertedLines.length; i++) {
-		const line = [convertedLines[i]];
-		const numbers = line[0].split("").filter(isNumeric);
-		if (numbers.length > 2) {
-			numbers.splice(1, numbers.length - 2);
+	let total = 0;
+	for (const line of lines) {
+		const digits = [];
+		for (const [index, char] of line.split("").entries()) {
+			if (isDigit(char)) {
+				digits.push(char);
+			}
+			for (const [digit, value] of [
+				"one",
+				"two",
+				"three",
+				"four",
+				"five",
+				"six",
+				"seven",
+				"eight",
+				"nine",
+			].entries()) {
+				if (line.substring(index).startsWith(value)) {
+					digits.push((digit + 1).toString());
+				}
+			}
 		}
-		const numberArray = numbers.map(Number);
-		if (numberArray.length < 2) {
-			numberArray.push(numberArray[0]);
-		}
-		const combinedNumber = parseInt(numberArray.join(""));
-		if (!Number.isNaN(combinedNumber)) {
-			allNumbers.push(combinedNumber);
-		}
+		const calibratedValue = parseInt(digits[0] + digits.at(-1));
+		total += calibratedValue;
 	}
-	const sum = allNumbers.reduce(
-		(accumulator, currentValue) => accumulator + currentValue,
-		0,
-	);
-	return sum;
+	return total;
 };
 
 console.log(processInput(input));

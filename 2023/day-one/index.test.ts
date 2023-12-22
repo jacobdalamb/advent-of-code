c32dfd563493d6d3d51b3fbd834c21d403579617
Jacob Lamb <jacobtlamb@hey.com>
2023-12-22 09:20:49 -0800

    Consolidate tests

--- 2023/day-one/index.test.ts
+++ 2023/day-one/index.test.ts
@@ -3,14 +3,11 @@ import { processInput } from "./index";
 import partOneSubset from "./part-one-subset.txt";
 import partTwoSubset from "./part-two-subset.txt";
 
-test("processPartOneSubset", () => {
-	const input = partOneSubset;
-	const result = processInput(input);
-	expect(result).toBe(142);
-});
-
-test("processPartTwoSubset", () => {
-	const input = partTwoSubset;
-	const result = processInput(input);
-	expect(result).toBe(281);
+test("processInput", () => {
+	const firstInput = partOneSubset;
+	const secondInput = partTwoSubset;
+	const firstResult = processInput(firstInput);
+	const secondResult = processInput(secondInput);
+	expect(firstResult).toBe(142);
+	expect(secondResult).toBe(281);
 });
